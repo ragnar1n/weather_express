d59596ba6772c544cd58d28f0e8f47f9775c16ea
Ragnar <ragnar.naruski@voco.ee>
2022-12-16 12:49:28 +0200

    Created API error handling

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ const getWeatherDataPromise=(url)=>{
                 let result={
                     description:description,
                     city:city,
-                    temp:temp
+                    temp:temp,
+                    error:null
                 }
                 resolve(result)
             })
@@ -48,6 +49,9 @@ app.all('/',function (req,res){
     getWeatherDataPromise(url)
         .then(data=>{
             res.render('index',data)
+        })
+        .catch(error=>{
+            res.render('index',{error:'Problem with getting data, try again'})
         })
 })
 
