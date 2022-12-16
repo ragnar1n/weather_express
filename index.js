eda15cfb6fb3850255c2d7b3ae4e8ccf83484084
Ragnar <ragnar.naruski@voco.ee>
2022-12-16 12:35:31 +0200

    Refactored code dublicate with Promice for data fetching

--- index.js
+++ index.js
@@ -2,27 +2,52 @@ const express=require('express')
 const app= express()
 const path=require('path')
 const fetch=require('node-fetch')
+const {response} = require("express");
 
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 
 const key='6cec3250795104e5b0a08a43046cdb28'
-let city='Tartu'
 
-app.get('/',function (req,res){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-        .then(function (resp){
-            return resp.json()
-        })
-        .then(function (data){
-            let description=data.weather[0].description
-            let city=data.name
-            let temp=Math.round(parseFloat(data.main.temp)-273.15)
-            res.render('index',{
-                description:description,
-                city:city,
-                temp:temp
+const getWeatherDataPromise=(url)=>{
+    return new Promise((resolve,reject)=>{
+        fetch(url)
+            .then(response=>{
+                return response.json()
             })
+            .then(data=>{
+                let description=data.weather[0].description
+                let city=data.name
+                let temp=Math.round(parseFloat(data.main.temp)-273.15)
+                let result={
+                    description:description,
+                    city:city,
+                    temp:temp
+                }
+                resolve(result)
+            })
+            .catch(error=>{
+            reject(error)
+        })
+    })
+}
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.all('/',function (req,res){
+    let city
+    if (req.method=='GET'){
+        city='Tartu'
+    }
+    if (req.method=='POST'){
+        city=req.body.cityname
+    }
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+    getWeatherDataPromise(url)
+        .then(data=>{
+            res.render('index',data)
         })
 })
 
